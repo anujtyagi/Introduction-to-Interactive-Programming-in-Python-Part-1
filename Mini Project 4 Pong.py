83128b8271699d1bc5bcf1616ef892e007156b04
wy123123 <wangyu0917@hotmail.com>
2015-03-19 11:05:07 +0800

    Update Mini Project 4 Pong.py

--- Mini Project 4 Pong.py
+++ Mini Project 4 Pong.py
@@ -110,7 +110,7 @@ frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 frame.set_keyup_handler(keyup)
-button2 = frame.add_button('Restart',new_game, 50)
+button2 = frame.add_button('Restart',new_game, 70)
 # start frame
 new_game()
 frame.start()
